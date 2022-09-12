9c92bd7330e68d43f18ff2f796f67693237edd5e
usha867 <sonalighodake867>
2022-09-12 16:30:31 +0530

    arrays operation

---
+++ arrays.js
@@ -0,0 +1,22 @@
+let data=[5,3,8,2,9,1];
+
+console.log("elements:",data);
+// push operation
+console.log(data.push(4));
+console.log("after push:",data);
+// pop operation
+console.log(data.pop());
+console.log("after pop:",data);
+// shift (deleted first  index element)
+console.log(data.shift());
+console.log("after shift:",data);
+
+// unshift (add the number at first indexx)
+console.log(data.unshift(0));
+console.log("after unshift",data);
+
+// indexof
+console.log(data.indexOf(9));
+console.log("index:",data);
+
+
